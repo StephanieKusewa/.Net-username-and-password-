ec9d94b8c65f3b5c9916b2fa96540e835446e4a4
StephanieKusewa <86594166+StephanieKusewa@users.noreply.github.com>
2023-02-13 22:00:06 +0300

    Add files via upload

---
+++ login.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="class1.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+        <br />
+        <br />
+        <asp:Label ID="lbOutput" runat="server" Text="lbOutput"></asp:Label>
+        <br />
+        <br />
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ login.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace class1
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String username,   password;
+            username = txtUsername.Text;
+            password = (txtPassword.Text);
+            if ( username == "user" && password == "12345")
+            {
+                lbOutput.Text = "Right login credentials";
+                lbOutput.ForeColor = System.Drawing.Color.Green;
+            }
+                else
+            {
+                lbOutput.Text = "Wrong login credentials";
+                lbOutput.ForeColor= System.Drawing.Color.Red;
+            }
+
+
+
+        }
+    }
+}
